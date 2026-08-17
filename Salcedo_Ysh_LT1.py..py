e987181fea89eaabae598e802c58f7fc3fbe42d5
YshSalcedo <yasalcedo@brc.pshs.edu.ph>
2026-08-17 11:06:19 +0800

    Add files via upload

--- Salcedo_Ysh_LT1.py..py
+++ Salcedo_Ysh_LT1.py..py
@@ -1,13 +1,14 @@
-#There are specific scenarios that requires specific handling when standard procedures fail.
+# A calculator for the total number of filled boxes and loose notebooks
 
 n = int(input("Total Number of Notebooks:"))
 t = int(input("How many notebooks fit in one box:"))
 
-full_boxes = n//t
-loose_notebooks = n%t
+full_boxes = n//t # Gives the total number of full boxes
+loose_notebooks = n%t # Gives the total number of loose notebooks
 
 print ("There are", full_boxes, "full boxes")
 print ("There are", loose_notebooks, "loose notebooks")
 
+# A conditional statement added when the number of notebooks is less than the box's capacity. 
 if n<t:
     print ("no full box was filled.")
